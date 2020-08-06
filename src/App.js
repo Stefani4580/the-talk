0d811ec0245cfe4544ba77fbbf4af636997f8f9d
stefanibaker <its.stefani@gmail.com>
2020-08-06 14:40:29 -0400

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -10,17 +11,27 @@ import Button from "react-bootstrap/Button";
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar bg="light" expand="lg">
         <Navbar.Brand href="#home">The Talks</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
             <Nav.Link href="#home">Interview Directory</Nav.Link>
             <Nav.Link href="#link">World Guide</Nav.Link>
-            <Nav.Link href="#link"><i class="fab fa-facebook-f"></i></Nav.Link>
-            <Nav.Link href="#link"><i class="fab fa-twitter"></i></Nav.Link>
-            <Nav.Link href="#link"><i class="fab fa-instagram-square"></i></Nav.Link>
-            
+            <Nav.Link href="https://www.facebook.com/TheTalksInterviews/">
+              <i class="fab fa-facebook-f"></i>
+            </Nav.Link>
+            <Nav.Link href="https://twitter.com/the_talks">
+              <i class="fab fa-twitter"></i>
+            </Nav.Link>
+            <Nav.Link href="https://www.instagram.com/thetalks/link">
+              <i class="fab fa-instagram-square"></i>
+            </Nav.Link>
+
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -39,6 +50,18 @@ function App() {
           </Form>
         </Navbar.Collapse>
       </Navbar>
+      <Switch>
+          {/* <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route> */}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
